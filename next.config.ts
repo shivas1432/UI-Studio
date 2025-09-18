96a39491fb9c1107a815762cfa73950ddd3edc98
shivas1432 <shivashanker7337@gmail.com>
2025-09-18 16:04:03 +0100

    Add missing dependencies for components

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
-  }
+  },
+  swcMinify: false,
 };
 
 export default nextConfig;
